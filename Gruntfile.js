d613dde9061e9dd9f3810d84165f852dfa67f786
edison <edison@chedaya.mshanken.com>
2016-03-10 16:08:40 -0500

    thembuilder tested

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
         style: './_site/public/css',
         images: './_site/public/images',
         script: './_site/public/js',
-        font: './_site/public/font'
+        font: './_site/public/fonts'
       },
       prod: {
         root: 'www',
